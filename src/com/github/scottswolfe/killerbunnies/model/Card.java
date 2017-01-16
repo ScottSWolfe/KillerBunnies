212066b5d0f49bd90b4016e17096a9ef2d4e0e4f
Scott Wolfe <swolfe8@gatech.edu>
2017-01-16 18:56:53 -0500

    First Commit(ish)!

---
+++ src/com/github/scottswolfe/killerbunnies/model/Card.java
@@ -0,0 +1,13 @@
+package com.github.scottswolfe.killerbunnies.model;
+
+public abstract class Card {
+
+    /**
+     * The name of the card.
+     */
+    private String name;
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
