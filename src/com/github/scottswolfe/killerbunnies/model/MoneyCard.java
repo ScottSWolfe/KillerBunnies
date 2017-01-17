0b1a127e7e8c3ba26ed17c76c5cbe332ec36d6ce
home <home@home-PC>
2017-01-16 20:48:22 -0500

    added money and very special cards

---
+++ src/com/github/scottswolfe/killerbunnies/model/MoneyCard.java
@@ -0,0 +1,10 @@
+package com.github.scottswolfe.killerbunnies.model;
+
+public class MoneyCard extends PlacedCard{
+
+	public MoneyCard(int cardNumber, String deckColor) {
+		super(cardNumber, deckColor);
+		// TODO Auto-generated constructor stub
+	}
+
+}
