77e5c9f71c54da06c1e3d59f55ceeff5ecc4fc56
Scott Wolfe <swolfe8@gatech.edu>
2017-01-16 20:47:22 -0500

    added PlayImmediatelyCard class

---
+++ src/com/github/scottswolfe/killerbunnies/model/PlayImmediatelyCard.java
@@ -0,0 +1,10 @@
+package com.github.scottswolfe.killerbunnies.model;
+
+public class PlayImmediatelyCard extends PlacedCard {
+
+    public PlayImmediatelyCard(int cardNumber, String deckColor) {
+        super(cardNumber, deckColor);
+        // TODO Auto-generated constructor stub
+    }
+
+}
