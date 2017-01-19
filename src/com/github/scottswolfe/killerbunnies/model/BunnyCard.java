14be4a5d84c57187c6f791e925d12c02646d1ad4
home <home@home-PC>
2017-01-18 20:43:21 -0500

    added weapon cards and bunnies

---
+++ src/com/github/scottswolfe/killerbunnies/model/BunnyCard.java
@@ -0,0 +1,10 @@
+package com.github.scottswolfe.killerbunnies.model;
+
+public class BunnyCard extends RunCard{
+
+	public BunnyCard(int cardNumber, String deckColor) {
+		super(cardNumber, deckColor);
+		// TODO Auto-generated constructor stub
+	}
+
+}
