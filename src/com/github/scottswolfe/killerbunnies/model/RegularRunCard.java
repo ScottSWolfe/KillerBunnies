27e9a33611cf44bb2591c714087cd835fe2c955d
home <home@home-PC>
2017-01-18 21:09:44 -0500

    added choose a carrot, feed the bunny and regular run card

---
+++ src/com/github/scottswolfe/killerbunnies/model/RegularRunCard.java
@@ -0,0 +1,11 @@
+package com.github.scottswolfe.killerbunnies.model;
+
+public class RegularRunCard extends RunCard{
+	
+	public RegularRunCard(int cardNumber, String deckColor)
+		{super(cardNumber,deckColor);
+	
+	}
+	
+
+}
