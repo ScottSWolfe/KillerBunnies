2c89f192552e19b154e4f7a9becdbdd380741e87
Scott Wolfe <swolfe8@gatech.edu>
2017-01-19 21:03:48 -0500

    created dice class

---
+++ src/com/github/scottswolfe/killerbunnies/item/dice/Dice.java
@@ -0,0 +1,20 @@
+package com.github.scottswolfe.killerbunnies.item.dice;
+
+public class Dice {
+
+    // Instance Variables
+    
+    // Constructor
+    
+    
+    
+    
+    
+    // Methods
+    public int roll() {
+   
+        // look up the java class Random in the java.util package
+    }
+   
+    
+}
