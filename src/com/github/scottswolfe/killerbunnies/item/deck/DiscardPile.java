ff27053e6b643d7c931955542b70d209a259b195
Scott Wolfe <swolfe8@gatech.edu>
2017-01-19 21:24:57 -0500

    added deck package with DiscardPile and DrawPile

---
+++ src/com/github/scottswolfe/killerbunnies/item/deck/DiscardPile.java
@@ -0,0 +1,5 @@
+package com.github.scottswolfe.killerbunnies.item.deck;
+
+public class DiscardPile {
+
+}
---
+++ src/com/github/scottswolfe/killerbunnies/item/deck/DrawPile.java
@@ -0,0 +1,33 @@
+package com.github.scottswolfe.killerbunnies.item.deck;
+
+public class DrawPile {
+
+    // Instance Variables
+    // should at least have some data structure containing PlayableCards
+    // I'd recommend you look up the java.util.LinkedList class
+     
+    
+    // Constructor
+    // maybe don't worry about this yet...
+    
+    
+    
+    // Methods
+    
+    /**
+     * Removes and returns the top card of the DrawPile
+     */
+    public PlayableCard draw() {
+        
+    }
+    
+    
+    
+    /**
+     * Randomly shuffles the cards and returns shuffled DrawPile
+     */
+    public DrawPile shuffle() {
+        
+    }
+    
+}
