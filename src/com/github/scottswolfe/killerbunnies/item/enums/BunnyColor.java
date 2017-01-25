0f91fa507b6f9efae1893d465314aa99bee60612
Scott Wolfe <swolfe8@gatech.edu>
2017-01-24 21:03:53 -0500

    Added enums

---
+++ src/com/github/scottswolfe/killerbunnies/item/enums/BunnyColor.java
@@ -0,0 +1,8 @@
+package com.github.scottswolfe.killerbunnies.item.enums;
+
+public enum BunnyColor {
+    
+    BLUE, VIOLET, ORANGE, GREEN, YELLOW, FREE_AGENT,
+    RED, PINK, SPECIALTY, CELEBRITY
+    
+}
---
+++ src/com/github/scottswolfe/killerbunnies/item/enums/DeckColor.java
@@ -0,0 +1,8 @@
+package com.github.scottswolfe.killerbunnies.item.enums;
+
+public enum DeckColor {
+
+    BLUE, YELLOW, RED, VIOLET, ORANGE, GREEN, TWILIGHT_WHITE, STAINLESS_STEEL,
+    PERFECTLY_PINK, WACKY_KHAKI, OMINOUS_ONYX, CHOCOLATE
+    
+}
