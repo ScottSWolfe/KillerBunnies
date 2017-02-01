4a693dfbb3e36727e620f4ba45688830da329060
Scott Wolfe <swolfe8@gatech.edu>
2017-01-31 20:47:44 -0500

    minor change

--- src/com/github/scottswolfe/killerbunnies/item/card/CabbageCard.java
+++ src/com/github/scottswolfe/killerbunnies/item/card/CabbageCard.java
@@ -2,5 +2,8 @@ package com.github.scottswolfe.killerbunnies.item.card;
 
 public class CabbageCard extends ResourceCard {
 
-    
+    public CabbageCard(String name, int value) {
+        super(name, value);        
+    }
+
 }
